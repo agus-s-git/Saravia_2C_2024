3bef0ca988fb398af2a769bbbe790c94c798202f
Agustín Saravia <agustin.saravia@ingenieria.uner.edu.ar>
2024-10-10 19:47:25 -0300

    Se sube la documentación de la guia 2 ejercicio 3

--- firmware/projects/guia2_ej3/main/guia2_ej3.c
+++ firmware/projects/guia2_ej3/main/guia2_ej3.c
@@ -1,28 +1,37 @@
-/*! @mainpage Template
+/*! @mainpage Guía 2 - Ejercicio 3
  *
- * @section genDesc General Description
+ * @section Descripción
  *
- * This section describes how the program works.
+ * El programa modifica la actividad 2 agregando
+ * un puerto serie. Se envían datos de las mediciones
+ * para que puedan ser observados en un terminal en 
+ * la PC. Además, con las teclas "O" y "H" se enciende
+ * y se mantiene la medición respectivamente.
  *
- * <a href="https://drive.google.com/...">Operation Example</a>
+ * 
  *
  * @section hardConn Hardware Connection
  *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | Peripheral  | ESP32        |
+ * |-------------|--------------|
+ * |  HC-SR04 Trig | GPIO_3     |
+ * |  HC-SR04 Echo | GPIO_2     |
+ * |  LED 1      | GPIO_20      |
+ * |  LED 2      | GPIO_21      |
+ * |  LED 3      | GPIO_22      |
+ * |  Switch 1   | GPIO_4      |
+ * |  Switch 2   | GPIO_15      |
  *
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 03/10/2023 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Agustín Saravia (agustin.saravia@ingenieria.uner.edu.ar)
  *
  */
-
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
@@ -37,23 +46,56 @@
 #include "timer_mcu.h"
 #include "uart_mcu.h"
 /*==================[macros and definitions]=================================*/
+/**
+ * @def CONFIG_BLINK_PERIOD_LED
+ * @brief Periodo de parpadeo de los LEDs en milisegundos 
+*/ 
 #define CONFIG_BLINK_PERIOD_LED 1000
+/**
+ * @def DELAY_MOSTRAR
+ * @brief Delay para mostrar la distancia en la pantalla LCD  
+ */
+
 #define DELAY_MOSTRAR 500
+/**
+ * @def DELAY_MEDIR
+ * @brief Intervalo de tiempo para medir la distancia  
+ */
 #define DELAY_MEDIR 1000
+/**
+ * @def DELAY_TECLAS
+ * @brief Intervalo de tiempo para leer las teclas  
+ */
 #define DELAY_TECLAS 100
+/**
+ * @def CONFIG_PERIOD_SWITCH
+ * @brief  Período de configuración para el temporizador de las teclas
+ */
 #define CONFIG_PERIOD_SWITCH 1000000
 
+/*! @brief Variable para almacenar la distancia medida en centímetros */
 uint16_t DISTANCIA;
+/*! @brief Variable que indica si el sistema está encendido */
 bool ON;
+/*! @brief Variable que indica si el sistema está en modo "HOLD" */
 bool HOLD;
 
 /*==================[internal data definition]===============================*/
+/*! @brief Manejador de la tarea de mostrar */
 TaskHandle_t mostrar_task_handle = NULL;
+/*! @brief Manejador de la tarea de medición de distancia */
 TaskHandle_t medir_task_handle = NULL;
+/*! @brief Manejador de la tarea de teclas */
 TaskHandle_t teclas_task_handle = NULL;
+/*! @brief Manejador de la tarea del control de la UART */
 TaskHandle_t uart_task_handle = NULL;
 /*==================[internal functions declaration]=========================*/
 
+/**
+ * @fn void detectarTeclas(void *param)
+ * @brief Función para detectar lo que se envía por el serial monitor ("O" o "H")
+ * @param param Parametro
+ */
 void detectarTeclas(void *param){
 	uint8_t tecla;
 	UartReadByte(UART_PC, &tecla);
@@ -70,30 +112,29 @@ void detectarTeclas(void *param){
 		break;
 	}
 }
-
+/**
+ * @fn static void manejarLEDs()
+ * @brief Función que controla el encendido de los LEDs según la distancia medida.
+ */
 static void manejarLEDs(){
 	
 		if (DISTANCIA<10)
 		{
 			LedsOffAll();
 			
-			
 		}
 		else if (DISTANCIA >= 10 && DISTANCIA <= 20)
 		{
 			LedOn(LED_1);
 			
-			
 		}
 		else if (DISTANCIA >= 20 && DISTANCIA <= 30)
 		{
-			
 			LedOn(LED_1);
 			LedOn(LED_2);
 		}
 		else if (DISTANCIA >= 30)
-		{
-			
+		{	
 			LedOn(LED_1);
 			LedOn(LED_2);
 			LedOn(LED_3);
@@ -101,7 +142,11 @@ static void manejarLEDs(){
 	
 
 }
-//Listo
+/**
+ * @fn static void tareaMedir(void *pvParameter)
+ * @brief Tarea que mide la distancia utilizando el sensor HC-SR04.
+ * @param pvParameter Parámetro de FreeRTOS
+ */
 static void tareaMedir(void *pvParameter){
 	while (true)
 	{
@@ -113,20 +158,42 @@ static void tareaMedir(void *pvParameter){
 	}
 	
 }
+/**
+ * @fn void FuncTimerTeclas(void* param)
+ * @brief Función asociada al temporizador para activar la lectura de teclas.
+ * @param param Parametro no utilizado
+ */
 void FuncTimerTeclas(void* param){
     vTaskNotifyGiveFromISR(medir_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al LED_1 */
 }
+/**
+ * @fn void FuncTimerMedir(void* param)
+ * @brief Función asociada al temporizador para activar la medición de distancia.
+ * @param param Parametro no utilizado
+ */
 void FuncTimerMedir(void* param){
     vTaskNotifyGiveFromISR(mostrar_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al LED_1 */
 	vTaskNotifyGiveFromISR(uart_task_handle, pdFALSE);
 }
+/**
+ * @fn void tecla1()
+ * @brief Función que alterna el estado del sistema (ON/OFF) al presionar la tecla 1.
+ */
 void tecla1(){
 	ON = !ON;
 }
+/**
+ * @fn void tecla1()
+ * @brief Función que alterna el estado del sistema (ON/OFF) al presionar la tecla 2.
+ */
 void tecla2(){
 	HOLD = !HOLD;
 }
-
+/**
+ * @fn static void tareaMostrarDistancia(void *pvParameter)
+ * @brief Tarea para mostrar la distancia en el LCD y controlar el encendido de los LEDs.
+ * @param pvParameter Parámetro no utilizado.
+ */
 static void tareaMostrarDistancia(void *pvParameter){
 	
 	
@@ -150,7 +217,11 @@ static void tareaMostrarDistancia(void *pvParameter){
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	}
 }
-
+/**
+ * @fn void tareaUart(void *pvParameter)
+ * @brief Tarea que envia la distancia medida por la UART.
+ * @param pvParameter Parámetro no utilizado.
+ */
 void tareaUart(void *pvParameter){
 	while(true){
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
